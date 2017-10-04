e0d09353d175af38c8769a5883d50b652d61d0bf
Сергей Рогачев <redknife@ip-192-168-1-198.is74.loc>
2017-10-04 16:14:08 +0500

    Update zshrc, envs and aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,13 +1,10 @@
 alias a='atom'
 
-alias dev='cd ~/Developer/rs'
-alias go-work='cd ~/Developer/rs/work'
-alias go-testing='cd ~/Developer/rs/testing'
-alias go-gh='cd ~/Developer/rs/github'
-alias go-bb='cd ~/Developer/rs/bitbucket'
-alias go-rs='cd ~/Developer/rs'
-alias go-dotfiles='cd ~/Developer/rs/dotfiles'
-alias go-ctf='cd ~/Developer/rs/ructf'
+alias dev='cd ~/dev'
+alias go-work='cd ~/dev/work'
+alias go-pg='cd ~/dev/playgrounds'
+alias go-gh='cd ~/dev/github'
+alias go-dotfiles='cd ~/dev/dotfiles'
 
 # Simple http server in cur dir
 alias serve='python -m SimpleHTTPServer'
--- zsh/envs.zsh
+++ zsh/envs.zsh
@@ -1,7 +1,4 @@
 export HOMEBREW_NO_ANALYTICS=1
 
-# Editor
-export EDITOR='atom'
-
 # Homebrew
-export PATH="/usr/local/sbin:$PATH:$(yarn global bin)"
+# export PATH="/usr/local/sbin:$PATH:$(yarn global bin)"
